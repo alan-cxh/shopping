6e2de99de8c4b1a29379f4e856257b92742d0616
alan_chen <cxh_928765@163.com>
2019-12-15 23:22:46 +0800

    first

--- shopping/shopping-sso/shopping-sso-server/src/main/java/com/example/shopping/sso/server/SsoServerApplication.java
+++ shopping/shopping-sso/shopping-sso-server/src/main/java/com/example/shopping/sso/server/SsoServerApplication.java
@@ -1,15 +1,13 @@
-package com.example.shopping.center;
-
+package com.example.shopping.sso.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class CenterApplication {
-
+@EnableDiscoveryClient
+public class SsoServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CenterApplication.class, args);
+        SpringApplication.run(SsoServerApplication.class, args);
     }
 }
---
+++ shopping/shopping-sso/shopping-sso-server/src/main/java/com/example/shopping/sso/server/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.shopping.sso.server.controller;
+
+public class UserController {
+}
